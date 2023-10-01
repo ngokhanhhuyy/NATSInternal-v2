026d957cd49513dd873757dd7bcade30e1cf39c3
nh0zhuybmt <93859822+nh0zhuybmt@users.noreply.github.com>
2023-10-01 20:23:30 +0900

    Modifying attribute "app" in server

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from app import application
+from app import application as app
 
 def main():
     # application.run(host="0.0.0.0", port="5000")
-    application.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)
 
 if __name__ == "__main__":
     main()
