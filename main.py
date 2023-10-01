670dba1e5c8ca992ca22b26974e18bd4d75b75da
nh0zhuybmt <93859822+nh0zhuybmt@users.noreply.github.com>
2023-10-01 20:01:07 +0900

    Adjusted application starting statement

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from app import application
 
 def main():
     # application.run(host="0.0.0.0", port="5000")
-    application.run(host="127.0.0.1", port="5000")
+    application.run(debug=True, port=5000)
 
 if __name__ == "__main__":
     main()
