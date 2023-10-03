a250363f23723a0631b33163a2b5b39f241783b5
nh0zhuybmt <93859822+nh0zhuybmt@users.noreply.github.com>
2023-10-04 00:27:31 +0900

    Added Flask_SQLAlchemy extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.config import Config
+from app.config import Config, ProductionConfig, DevelopmentConfig
 from flask_bcrypt import Bcrypt
 import os
 
@@ -8,9 +8,15 @@ application = Flask(
     __name__,
     template_folder="views")
 bcrypt = Bcrypt(application)
-
 # Define application's configurations
 application.config.from_object(Config)
+# Determine database connection string
+if os.getenv("FLASK_ENV") == "development":
+    application.config["SQLALCHEMY_DATABASE_URI"] = DevelopmentConfig.SQLALCHEMY_DATABASE_URI
+elif os.getenv("FLASK_ENV") == "production":
+    application.config["SQLALCHEMY_DATABASE_URI"] = ProductionConfig.SQLALCHEMY_DATABASE_URI
+else:
+    raise ValueError
 application.json.sort_keys = False
 
 # Routes
--- app/data/__init__.py
+++ app/data/__init__.py
@@ -1,36 +1,18 @@
 from app import application
 from flask import g as requestContext
-from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
-from sqlalchemy.orm import scoped_session, sessionmaker, Session
-from sqlalchemy.sql import quoted_name
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import scoped_session, sessionmaker, Session, DeclarativeBase
 from app import application
 from app.config import DevelopmentConfig, ProductionConfig
 from contextlib import contextmanager
 import os
 from typing import Type, Generator
 
-# Determine database connection string
-if os.getenv("FLASK_ENV") == "development":
-    databaseURI = DevelopmentConfig.SQLALCHEMY_DATABASE_URI
-elif os.getenv("FLASK_ENV") == "production":
-    databaseURI = ProductionConfig.SQLALCHEMY_DATABASE_URI
-else:
-    raise ValueError("FLASK_ENV variable value is invalid.")
-# Create a session
-engine = create_engine(
-    databaseURI,
-    connect_args={"options": "-c timezone=Asia/Ho_Chi_Minh"})
-engine.dialect.identifier_preparer._double_percents = True
-engine.dialect.identifier_preparer._double_quote = True
-engine.dialect.identifier_preparer._quote = lambda value, identifier: quoted_name(value, identifier)
-ScopeSession = scoped_session(sessionmaker(
-    expire_on_commit=True,
-    autocommit=False,
-    autoflush=False,
-    bind=engine))
-Base: DeclarativeMeta = declarative_base()
-metadata = Base.metadata
+# Initializing base class for models
+class Base(DeclarativeBase):
+    pass
+
+database = SQLAlchemy(application, model_class=Base)
 
 def initializeDatabase():
     from app.models.activity import Activity
@@ -62,23 +44,20 @@ def initializeDatabase():
     from app.models.treatment_item import TreatmentItem
     from app.models.treatment_payment import TreatmentPayment
     from app.models.photo import Photo
-    Base.metadata.create_all(bind=engine)
 
 def getDatabaseSession() -> Session:
-    if "databaseSession" not in requestContext:
-        requestContext.databaseSession = ScopeSession()
-    return requestContext.databaseSession
+    return database.session
 
 # @application.before_request
 # def creatingDatabaseSession():
 #     """Creating a new database session for each request."""
 #     requestContext.databaseSession: scoped_session[Session] = ScopeSession()
 
-@application.teardown_appcontext
-def destroyingDatabaseSession(exception: Exception = None):
-    """Destroying existing database session when the request - response circle ended."""
-    ScopeSession.close_all()
-    engine.dispose()
+# @application.teardown_appcontext
+# def destroyingDatabaseSession(exception: Exception = None):
+#     """Destroying existing database session when the request - response circle ended."""
+#     ScopeSession.close_all()
+#     engine.dispose()
 
 @contextmanager
 def getTemporaryDatabaseSession() -> Generator[Session, None, None]:
